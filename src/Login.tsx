8e4e1d9902f7b3beb6d2b3b40ce470d197abb80e
Mikael Ruffieux <ruffieux.mikael@gmail.com>
2022-04-08 11:30:06 +0200

    Implement API Communication on Login cmpn

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,12 +7,13 @@ interface Props {}
 interface State {
   validMail: boolean;
   validPsw: boolean;
+  errorMsg: string;
 }
 
 export default class Login extends React.Component<Props, State> {
   constructor(props: never) {
     super(props);
-    this.state = { validMail: false, validPsw: false };
+    this.state = { validMail: false, validPsw: false, errorMsg: '' };
     this.setEmailValidation = this.setEmailValidation.bind(this);
     this.setPswValidation = this.setPswValidation.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,7 +21,12 @@ export default class Login extends React.Component<Props, State> {
 
   handleSubmit(evt: any) {
     evt.preventDefault();
-    console.log(this.state.validMail && this.state.validPsw ? 'submiting' : 'no no no');
+    if (this.state.validMail && this.state.validPsw) {
+      // Static for dev purpose
+      this.postCredentials('noemail@noemail.ch', 'password');
+    } else {
+      this.setState({ errorMsg: 'Merci de remplir tous les champs.' });
+    }
   }
 
   setEmailValidation(valid: boolean): void {
@@ -31,10 +37,35 @@ export default class Login extends React.Component<Props, State> {
     this.setState({ validPsw: valid });
   }
 
+  postCredentials(mail: string, psw: string) {
+    const apiUrl = 'http://127.0.0.1:3333/login';
+    const init = {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({ email: mail, password: psw }),
+    };
+
+    console.log(init.body);
+
+    fetch(apiUrl, init)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.type === 'error') {
+          this.setState({ errorMsg: res.content });
+        } else {
+          localStorage.setItem('token', res.token);
+          this.context.router.push('/');
+        }
+      });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>Hello World</h1>
+        <small>{this.state.errorMsg || ''}</small>
         <MailInput setValidation={this.setEmailValidation} />
         <PswInput setValidation={this.setPswValidation} />
         <input type="submit" value="Login" />
