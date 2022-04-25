25e4a0118262ab64da72a19e82151aebaae6350e
Satche <info@satche.ch>
2022-04-25 10:06:09 +0200

    Update checkbox default value

--- src/input/form/Checkbox.tsx
+++ src/input/form/Checkbox.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormInputType } from '../../types/FormInputType';
 
 export default function Checkbox({ onChange, ...props }: FormInputType) {
+  useEffect(() => {
+    // Uncheck checkbox by default
+    if (props.value === undefined) {
+      onChange!(false);
+    }
+  });
+
+  // Trigger when the checkbox is checked/unchecked
   const handleChange = (event: any) => {
     onChange!(event.target.checked);
   };
 
+  // Template
   const displayView = () => (
     <div className="form__row">
       <input
         id={props.id}
         name={props.id}
         type="checkbox"
-        checked={props.value}
+        defaultChecked={props.value}
         onChange={handleChange}
       />
       <label htmlFor={props.id}>{props.label}</label>
