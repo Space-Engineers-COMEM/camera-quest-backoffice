6d76a3b75443268100bdb3af7237449313e4ca47
Satche <info@satche.ch>
2022-04-26 14:38:05 +0200

    Add go back link to submit button

--- src/input/form/Submit.tsx
+++ src/input/form/Submit.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router-dom';
 
 type SubmitProp = {
   title: string;
 };
 
 export default function Submit(props: SubmitProp) {
+  const navigator = useNavigate();
+
   /* Template */
   const displayView = () => (
-    <button type="submit" className="button button__modal">
+    <button type="submit" className="button button__modal" onClick={() => navigator(-1)}>
       {props.title}
     </button>
   );
